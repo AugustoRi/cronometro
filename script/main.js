efe5438ab73b951ab95f4ed5f258dd7a4c0f0e58
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2022-01-11 15:10:14 -0300

    finalizei os estados dos botoes

--- script/main.js
+++ script/main.js
@@ -1,4 +1,35 @@
 const buttonStart = document.querySelector('#start');
 const buttonStop = document.querySelector('#stop');
 const buttonRestart = document.querySelector('#restart');
+const unidades = [1,2,3,4,5,6,7,8,9];
+
+function ligarBotoes(botao) {
+  botao.enabled = true;
+  botao.disabled = false;
+
+  return botao;
+}
+
+function desligarBotoes(botao) {
+  botao.enabled = false;
+  botao.disabled = true;
+}
+
+function comecar() {
+  desligarBotoes(buttonStart);
+  ligarBotoes(buttonStop);
+  desligarBotoes(buttonRestart);
+};
+
+function parar() {
+  ligarBotoes(buttonStart);
+  desligarBotoes(buttonStop);
+  ligarBotoes(buttonRestart);
+}
+
+function zerar() {
+  ligarBotoes(buttonStart);
+  desligarBotoes(buttonStop);
+  desligarBotoes(buttonRestart);
+}
 
